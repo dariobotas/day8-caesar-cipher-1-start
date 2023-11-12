3f0ba777ee191968a227bc853ea9b576caa48440
dariobotas <dario.botas@gmail.com>
2023-11-12 21:14:04 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+#TODO-1: Combine the encrypt() and decrypt() functions into a single function called caesar(). 
+def caesar(direction_encryption, plain_text, shift_amount):
+  cipher_text = ""
+  if direction_encryption == 'decode':
+    shift_amount *= -1
+  for letter in plain_text:
+    position = alphabet.index(letter)
+#    if direction_encryption == 'encode':
+#      new_position = position + shift_amount
+#    else:
+#      new_position = position - shift_amount
+    new_position = position + shift_amount
+    cipher_text += alphabet[new_position]
+  print(f"The text that we {direction_encryption} is {cipher_text}")
+
+caesar(direction_encryption=direction, plain_text=text, shift_amount=shift)
